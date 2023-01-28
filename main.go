763b89f7208771ba93edbc2d2cb1eaaa3a108217
imaei <imaei@foxmail.com>
2023-01-29 00:51:50 +0800

    feat: main function comments

--- main.go
+++ main.go
@@ -1,11 +1,18 @@
 package main
 
-import "tiktok/boot"
+import (
+	"tiktok/boot"
+)
 
 func main() {
+	// 文件读取
 	boot.ViperSetup()
+	// 打印和日志功能  g.Logger
 	boot.LoggerSetup()
+	// mysql  g.MysqlDB
 	boot.MysqlDBSetup()
+	// redis  g.DbVerify
 	boot.RedisSetup()
+	// hertz  8080端口
 	boot.ServerSetup()
 }
