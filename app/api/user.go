692bf2dda0e383c001d19501a664366456255be1
imaei <imaei@foxmail.com>
2023-01-29 11:08:47 +0800

    feat: user初始化

---
+++ app/api/user.go
@@ -0,0 +1,12 @@
+package api
+
+import (
+	"context"
+	"github.com/cloudwego/hertz/pkg/app"
+	"github.com/cloudwego/hertz/pkg/common/utils"
+	"github.com/cloudwego/hertz/pkg/protocol/consts"
+)
+
+func GetUserInfo(c context.Context, ctx *app.RequestContext) {
+	ctx.JSON(consts.StatusOK, utils.H{"message": "pong"})
+}
