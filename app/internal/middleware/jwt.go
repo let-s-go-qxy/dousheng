96f2554934e6b072dc947c6dc0a7ba90ca091f75
imaei <imaei@foxmail.com>
2023-01-30 22:29:01 +0800

    feat: userInfo接口完善，社交关注列表，follow基础模块，gorm日志功能

--- app/internal/middleware/jwt.go
+++ app/internal/middleware/jwt.go
@@ -2,22 +2,23 @@ package middleware
 
 import (
 	"context"
+	"fmt"
 	"github.com/cloudwego/hertz/pkg/app"
 	"github.com/cloudwego/hertz/pkg/protocol/consts"
-	"strconv"
 	"tiktok/app/api"
 	"tiktok/app/internal/service"
 	"time"
 )
 
 func Jwt() app.HandlerFunc {
-	return func(ctx context.Context, c *app.RequestContext) {
-		token := c.Query("token")
-		if token2 := c.PostForm("token"); token2 != "" {
+	return func(c context.Context, ctx *app.RequestContext) {
+		fmt.Println("jwt")
+		token := ctx.Query("token")
+		if token2 := ctx.PostForm("token"); token2 != "" {
 			token = token2
 		}
 		if token == "" {
-			c.AbortWithStatusJSON(consts.StatusOK, api.Response{
+			ctx.AbortWithStatusJSON(consts.StatusOK, api.Response{
 				StatusCode: 1, StatusMsg: "User doesn't exist",
 			})
 			return
@@ -25,20 +26,20 @@ func Jwt() app.HandlerFunc {
 		claims, err := service.ParseToken(token)
 		if err != nil || claims.Expire < int(time.Now().Unix()) {
 			// TODO 这里应该是返回401，但是Demo中是这么写的，所以为了适配app故此
-			c.AbortWithStatusJSON(consts.StatusOK, api.Response{
+			ctx.AbortWithStatusJSON(consts.StatusOK, api.Response{
 				StatusCode: 1, StatusMsg: "User doesn't exist",
 			})
 			return
 		}
-		c.Set("user_id", claims.Id)
-		if id := c.Query("user_id"); id != "" {
-			if id != strconv.Itoa(claims.Id) {
-				c.AbortWithStatusJSON(consts.StatusOK, api.Response{
-					StatusCode: 1, StatusMsg: "User doesn't exist",
-				})
-				return
-			}
-		}
-		c.Next(ctx)
+		ctx.Set("user_id", claims.Id)
+		//if id := ctx.Query("user_id"); id != "" {
+		//	if id != strconv.Itoa(claims.Id) {
+		//		ctx.AbortWithStatusJSON(consts.StatusOK, api.Response{
+		//			StatusCode: 1, StatusMsg: "User doesn't exist",
+		//		})
+		//		return
+		//	}
+		//}
+		ctx.Next(c)
 	}
 }
