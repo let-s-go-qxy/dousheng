3e86dd08484782e3f7999d89afc799a15f4144b2
imaei <imaei@foxmail.com>
2023-01-30 11:39:44 +0800

    feat: jwt优化

--- app/internal/middleware/jwt.go
+++ app/internal/middleware/jwt.go
@@ -13,6 +13,15 @@ import (
 func Jwt() app.HandlerFunc {
 	return func(ctx context.Context, c *app.RequestContext) {
 		token := c.Query("token")
+		if token2 := c.PostForm("token"); token2 != "" {
+			token = token2
+		}
+		if token == "" {
+			c.AbortWithStatusJSON(consts.StatusOK, api.Response{
+				StatusCode: 1, StatusMsg: "User doesn't exist",
+			})
+			return
+		}
 		claims, err := service.ParseToken(token)
 		if err != nil || claims.Expire < int(time.Now().Unix()) {
 			// TODO 这里应该是返回401，但是Demo中是这么写的，所以为了适配app故此
@@ -21,6 +30,7 @@ func Jwt() app.HandlerFunc {
 			})
 			return
 		}
+		c.Set("user_id", claims.Id)
 		if id := c.Query("user_id"); id != "" {
 			if id != strconv.Itoa(claims.Id) {
 				c.AbortWithStatusJSON(consts.StatusOK, api.Response{
