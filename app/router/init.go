ae642203f4ec7b8056a755e7c30f22a9ca840798
LYLKnight <li330240647@163.com>
2023-01-30 22:08:12 +0800

    feat: 补充评论创建操作和修改操作

--- app/router/init.go
+++ app/router/init.go
@@ -27,8 +27,8 @@ func InitRouter(h *server.Hertz) {
 	publicGroup.GET("/publish/action", api.GetFollowerList)
 	loggedGroup.GET("/publish/list", api.PublishList)
 	publicGroup.GET("/favorite/list", api.GetFavoriteList)
-	publicGroup.GET("/comment/list", api.GetCommentList)      // 查看视频评论列表
-	loggedGroup.POST("/comment/action", api.GetCommentAction) // 修改视频评论
+	publicGroup.GET("/comment/list", api.GetCommentList)       // 查看视频评论列表
+	loggedGroup.POST("/comment/action", api.PostCommentAction) // 修改视频评论
 	loggedGroup.POST("/relation/action", api.PublishVideo)
 	publicGroup.GET("/relation/follow/list", api.GetFollowerList)
 	publicGroup.GET("/relation/follower/list", api.GetFollowerList)
