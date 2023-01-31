7fc22559e0506f0ff3587dcfac893e977f020f96
LYLKnight <li330240647@163.com>
2023-01-31 21:52:43 +0800

    feat

--- app/api/common.go
+++ app/api/common.go
@@ -55,3 +55,13 @@ type FavoriteListResponse struct {
 	StatusMsg  string  `json:"status_msg,omitempty"`
 	VideoList  []Video `json:"video_list"`
 }
+type CommentListResponse struct {
+	StatusCode  int32     `json:"status_code"`
+	StatusMsg   string    `json:"status_msg,omitempty"`
+	CommentList []Comment `json:"comment_list,,omitempty"`
+}
+type CommentResponse struct {
+	StatusCode int32   `json:"status_code"`
+	StatusMsg  string  `json:"status_msg,omitempty"`
+	Comment    Comment `json:"comment,,omitempty"`
+}
