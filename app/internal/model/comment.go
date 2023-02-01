0b2860b786dd4b085847ba3a94ccb346afe6a175
LYLKnight <li330240647@163.com>
2023-02-01 22:01:47 +0800

    feat: 测试功能返回值

--- app/internal/model/comment.go
+++ app/internal/model/comment.go
@@ -6,30 +6,36 @@ import (
 
 type Comment struct {
 	Id         int    `gorm:"primaryKey" json:"id"`
-	User       User   `json:"user"`
-	Content    string `json:"content"`
-	CreateTime string `json:"create_date"`
+	UserId     int    `json:"user_id"`
+	Content    string `json:"comment_text" gorm:"column:comment_text"`
+	CreateTime string `json:"create_time"`
 	VideoId    int    `json:"video_id"`
 	Cancel     int    `gorm:"default:1"`
 }
 
+// 根据视频id查询全部评论
 func FindCommentByVideo(id int) []Comment {
 	comments := make([]Comment, 0)
 	g.MysqlDB.Where("video_id = ?", id).Find(&comments)
 	return comments
 }
 
+// 根据评论的id查询返回对应评论
 func FindCommentById(id int) (comment Comment) {
 	g.MysqlDB.Where("id = ?", id).Find(&comment)
-	return comment
+	return
 }
 
 // comment表内插入对应评论
-func CreateComment(comment Comment) {
-	g.MysqlDB.Create(comment)
+func CreateComment(comment *Comment) (returnComment *Comment, err error) {
+	db := g.MysqlDB.Create(comment)
+	model := db.Statement.Model
+	returnComment = model.(*Comment)
+	return
 }
 
-// comment表内删除对应评论,输入参数为评论的id
-func DeleteComment(commentId int) {
-	g.MysqlDB.Model(&Comment{Id: commentId}).Update("cancel", "0")
+// comment表内删除对应评论,输入参数为comment整体
+func DeleteComment(comment *Comment) (err error) {
+	err = g.MysqlDB.Model(&Comment{}).Where("id =?", comment.Id).Update("cancel", "0").Error
+	return
 }
