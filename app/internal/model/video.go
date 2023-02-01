54ff42c785aff65e1ae3a319799d09e219dc6071
wjh <672428366@qq.com>
2023-02-01 23:25:34 +0800

    feat:router init

--- app/internal/model/video.go
+++ app/internal/model/video.go
@@ -10,7 +10,7 @@ const TableNameVideo = "video"
 // Video mapped from table <video>
 type Video struct {
 	Id          int32  `gorm:"column:id;primaryKey;autoIncrement:true" json:"id"`
-	Author      int32  `gorm:"column:author_id;not null" json:"author"`
+	Author      int32  `gorm:"column:author_id;not null" json:"author"` //用户Id author_id
 	PlayUrl     string `gorm:"column:play_url;not null" json:"play_url"`
 	CoverUrl    string `gorm:"column:cover_url;not null" json:"cover_url"`
 	PublishTime int32  `gorm:"column:publish_time;not null" json:"time"`
